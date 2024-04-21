13df90854fc075b9dcf54c81c1f4b90956c5484d
vladislav lis <lis.vladislav@gmail.com>
2024-04-21 18:46:59 +0300

    chore: remove redundant files/comments

--- src/main.js
+++ src/main.js
@@ -1,9 +1,3 @@
-/**
- * main.js
- *
- * Bootstraps Vuetify and other plugins then mounts the App`
- */
-
 // Plugins
 import { registerPlugins } from "@/plugins";
 
@@ -12,14 +6,13 @@ import App from "./App.vue";
 
 // Composables
 import { createApp } from "vue";
-import store from "./store"; // Import your Vuex store
-import vuetify from "./plugins/vuetify"; // Import Vuetify instance
+import store from "./store";
+import vuetify from "./plugins/vuetify";
 
 const app = createApp(App);
 
 registerPlugins(app);
 
-// Use Vuex store and Vuetify
 app.use(store).use(vuetify);
 
 app.mount("#app");
